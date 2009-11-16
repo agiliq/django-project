16db259ac4c83cbef3d27dd46d5406bab701af5c
Usware Technologies <github@uswaretech.com>
2009-11-16 10:23:30 +0530

    Added localsettings and gitignore

--- settings.py
+++ settings.py
@@ -1,42 +1,7 @@
-# Django settings for prajact project.
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-"""
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'shabda_prajact'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'shabda_prajact'             # Not used with sqlite3.
-DATABASE_PASSWORD = '1dc17c68'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-"""
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'prajact'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = '081084'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-"""
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'c:\test.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-"""
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
+try:
+    from localsettings import *
+except ImportError:
+    pass
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -98,10 +63,14 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 SMTP_HOST = 'localhost'
 SMTP_PORT = 25
 AUTH_PROFILE_MODULE = 'prefs.userprofile'
-MEDIA_ROOT = 'G:/prajact/files'
-MEDIA_URL = '/files/'
 PERSISTENT_SESSION_KEY = 'persistant'
 REDIRECT_FIELD_NAME = "next"
+
+try:
+    from localsettings import *
+except ImportError:
+    raise
+
 
 #TESTING
 #
